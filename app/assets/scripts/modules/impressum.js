14af7b42dc8595b20be01116b24128740fcd53e3
Oleksii Sapov <sapov@gmx.at>
2021-11-30 18:39:06 +0100

    tried to set up the impressum with js

--- app/assets/scripts/modules/impressum.js
+++ app/assets/scripts/modules/impressum.js
@@ -13,9 +13,22 @@ for (let i = 0; i < impressumLinks.length; i++) {
 
     impressumLinks[i].addEventListener("click", function () {
         document.querySelector(".main").style.display = "none";
-        document.querySelector("#impressum").style.display = "block";
-        document.querySelector(".impressum--footer").style.display = "none";
-        document.querySelector(".home").style.display = "block";
+        document.querySelector("impressum").style.display = "block";
+        document.querySelector(".main").style.display = "none";
+      
+
+    });
+}
+
+var homeLinks = document.querySelectorAll(".home");
+
+for (let i = 0; i < homeLinks.length; i++) {
+
+    homeLinks[i].addEventListener("click", function () {
+        document.querySelector(".main").style.display = "block";
+        document.querySelector("impressum").style.display = "none";
+      
+    
 
     });
 }
