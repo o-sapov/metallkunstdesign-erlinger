29b46f48a15921cd5429c598047c6bde1975168f
Oleksii Sapov <sapov@gmx.at>
2021-12-12 21:00:51 +0100

    fix problems with false viewport, adjusted a little bit for mobile view

--- app/assets/scripts/modules/carousel.js
+++ app/assets/scripts/modules/carousel.js
@@ -3,12 +3,24 @@ import $ from 'jquery';
 // var $ = require('jquery');
 function stopSliding() {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    var windowIsThin = window.matchMedia("(max-width:992px)").matches;
+    var windowIsThin = window.matchMedia("(max-width:768px)").matches;
 
     if (isMobile || windowIsThin) {
         $('.carousel').attr("data-bs-interval", "false");
+        // $('#images').attr("class", "container-sm");
     };
+
+    var tablet = window.matchMedia("(min-width:768px)").matches;
+
+    if (tablet) {
+        console.log('tablet');
+        // $('#images').attr("class", "container-md");
+    }
 }
 
 $(document).ready(stopSliding());
 
+document.body.style.webkitTransform =  'scale(1)';
+document.body.style.msTransform =   'scale(100)';
+document.body.style.transform = 'scale(1)';
+document.body.style.zoom = screen.logicalXDPI / screen.deviceXDPI;
